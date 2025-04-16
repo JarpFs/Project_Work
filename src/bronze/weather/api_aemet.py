e9c19c39e2e179fd8f3ff2019a2617bdeb260820
JarpFs <jarp200211@gmail.com>
2025-04-16 10:45:19 +0100

    Connect to AEMET

--- src/bronze/weather/api_aemet.py
+++ src/bronze/weather/api_aemet.py
@@ -1,24 +1,40 @@
 
 from common.logger.logger import get_logger
 from common.extract.base_extractors import ApiExtraction
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 logger = get_logger(__name__)
 
 
 def main() -> None:
 
+    api_key = os.getenv('KEY_AEMET')
+
     #URL de la API pública (JSONPlaceholder)
-    url = 'https://opendata.aemet.es/opendata/api/valores/climatologicos/inventarioestaciones/estaciones/C419X%20-%20Adeje'
+    url = 'https://opendata.aemet.es/opendata/api/valores/climatologicos/inventarioestaciones/estaciones/C419X%2520-%2520Adeje'
 
-    api_call = ApiExtraction(url)
+    headers = {
+        "accept": "application/json",
+        "api_key": api_key
+    }
 
-    status, response = api_call.extract()
+    api_call = ApiExtraction(
+        url
+    )
+
+    status, response = api_call.extract(
+        method='get',
+        headers = headers
+    )
 
     # Verificar que la solicitud fue exitosa
     if status == 200:
         # Convertir la respuesta JSON a un diccionario de Python
         data = response
-        logger.info(f"Datos recibidos: {data[:5]}")  # Imprimir los primeros 5 elementos
+        logger.info(f"Datos recibidos (primeras 5 claves): {list(data.items())[:5]}") # Imprimir los primeros 5 elementos
     else:
         logger.error(f"Error al hacer la solicitud. Código de estado: {response}")
 
--- src/common/extract/base_extractors.py
+++ src/common/extract/base_extractors.py
@@ -18,17 +18,39 @@ class ApiExtraction(Extractor):
     def __init__(self, url):
         self.url = url
 
-    def extract(self) -> tuple[int, Union[dict, list, None]]:
+    def request(self, method: str = "get", **kwargs) -> requests.Response:
+        method = method.lower()
+        if method not in {"get", "post", "put", "delete", "patch"}:
+            raise ValueError(f"Unsupported HTTP method: {method}")
+
+        return requests.request(method, self.url, **kwargs)
+
+    def extract(self,method: str = 'get', **kwargs) -> tuple[int, Union[dict, list, None]]:
         try:
-            response = requests.get(self.url)
+            response = self.request(method, **kwargs)
             response.raise_for_status() 
 
             logger.info(f"Succesfully conection from {self.url}")
+
+            if response.content and "application/json" in response.headers.get("Content-Type", ""):
+                try:
+                    data = response.json()
+                except ValueError as e:
+                    logger.warning(f"Could not decode JSON from {self.url}: {e}")
+                    data = None
+            else:
+                logger.warning(f"No JSON content in response from {self.url}")
+                data = None
             return response.status_code, response.json()
 
         except requests.RequestException as e:
 
             logger.error(f"Fail conection from {self.url} with this error {e}")
             return None, None
+
+
+         
+
+
 
 
