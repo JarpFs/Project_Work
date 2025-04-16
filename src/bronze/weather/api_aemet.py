61053400dcdc814201e1c84c16da2ca0398930ef
JarpFs <jarp200211@gmail.com>
2025-04-16 11:51:20 +0100

    Optimize

--- src/bronze/weather/api_aemet.py
+++ src/bronze/weather/api_aemet.py
@@ -9,56 +9,50 @@ import pandas as pd
 load_dotenv()
 
 logger = get_logger(__name__)
+    
 
 
 def main() -> None:
 
     api_key = os.getenv('KEY_AEMET')
 
-    #URL de la API pública (JSONPlaceholder)
+    #URL Aemet public
     url = 'https://opendata.aemet.es/opendata/api/valores/climatologicos/inventarioestaciones/estaciones/C419X'
 
+    #Headers
     headers = {
         "accept": "application/json",
         "api_key": api_key
     }
 
+    #API call with endpoint
     api_call = ApiExtraction(
         url
     )
 
+    #Extract information 
     status, response = api_call.extract(
         method='get',
         headers = headers
     )
 
-    # Verificar que la solicitud fue exitosa
-    if status == 200:
-        # Convertir la respuesta JSON a un diccionario de Python
-        data = response
-        logger.info(f"Datos recibidos (primeras 5 claves): {list(data.items())[:5]}") # Imprimir los primeros 5 elementos
-    else:
-        logger.error(f"Error al hacer la solicitud. Código de estado: {response}")
-        data = None
-        raise ValueError
-
-
-    url_data = data['datos']
+    #Obtain data information  from another url
+    url_data = response['datos']
 
     api_call_data = ApiExtraction(
         url_data
     )
 
-    headers_data = {
-        "accept": "application/json"    
-    }
+    try:
+        logger.info(f"Complete extract data AEMET API from:{url_data}")
+        status, data = api_call_data.extract(
+        ) 
+    except:
+        data = None
+        raise Exception
 
-    status, response = api_call_data.extract(
-        method='get',
-        headers= headers_data
-    )
 
-    print(response)
+    print(data)
 
 
 
